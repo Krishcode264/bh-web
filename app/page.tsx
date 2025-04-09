a074ecd21719c850a73f79ca8567ee1de598657c
Krishcode264 <krishnazade99@gmail.com>
2025-04-09 06:27:00 +0530

    all chnages

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,105 @@
 import Image from "next/image";
+import eventPhoto from "../public/e.png";
+import profilePhoto from "../public/p.png";
 
+import chatPhoto from "../public/up2.png";
+
+import homePhoto from "../public/h.png";
+import playStore from "../public/ps.png";
+import appleStore from "../public/as.png";
+import logo from "../public/icon2.png";
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen w-full bg-gradient-to-tr from-violet-200 to-violet-400 font-sans">
+      <div className="p-2 sm:absolute  mx-auto md:top-14 top-5 left-12 rounded-2xl bg-violet-300 w-28">
+        <Image className="w-22 h-22 rounded-xl" src={logo} alt="" />
+      </div>
+      <div className="max-w-4xl mx-auto px-4 py-12">
+    
+          <header className="text-center mb-12">
+            <h1 className="text-4xl md:text-5xl font-bold space-y-1 text-gray-600 funnel-sans">
+              Find Your People,  Make New Friends,  Share Your Moments.
+            </h1>
+            <p className="mt-4 text-lg text-gray-600">
+              Bold Hug helps you discover events, meet new people, and share
+              real-life moments.
+            </p>
+          </header>
+        
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <section className="justify-between gap-4 items-center flex flex-col md:flex-row p-4 rounded-3xl  bg-violet-300">
+          <span className="md:w-[50%] ">
+            <p className="mt-4 text-lg text-gray-700">
+              Bold Hug is your go-to app for discovering events and finding
+              companions — whether it's for travel, sports, food, or just
+              hanging out. Create or join events, share photos, and connect with
+              people near you who love doing the same things. Built for
+              real-life connection. Designed for shared experiences.
+            </p>
+          </span>
+          <div className=" px-3 py-2 w-[60%] rounded-2xl md:w-[30%]  h-full shadow-xl shadow-violet-400  ">
+            <h2 className=" text-center text-2xl font-semibold mb-4 font-mono text-gray-600">
+              Download Now (Coming soon)
+            </h2>
+            <div className="flex flex-col  md:flex-row   justify-center gap-4">
+              <Link
+             href={""}
+                className=" bg-violet-200  text-white px-6 py-3 rounded-2xl font-medium hover:bg-violet-300 transition"
+              >
+                <Image
+                  className="w-12 h-12 align-middle mx-auto"
+                  src={playStore}
+                  alt=""
+                />
+              </Link>
+              <Link
+              href={""}
+                className="bg-violet-200 text-white px-6 py-3 rounded-2xl  font-medium hover:bg-violet-300 transition"
+              >
+                <Image className="w-12 h-12 mx-auto" src={appleStore} alt="" />
+              </Link>
+            </div>
+          </div>
+        </section>
+
+        <section className="mb-12 flex mt-12 ">
+          <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={homePhoto}
+              alt="Screenshot 1"
+              className="rounded-xl shadow"
             />
-            Deploy now
-          </a>
+            <Image
+              src={eventPhoto}
+              alt="Screenshot 1"
+              className="rounded-xl shadow"
+            />
+            <Image
+              src={profilePhoto}
+              alt="Screenshot 2"
+              className="rounded-xl shadow"
+            />
+            <Image
+              src={chatPhoto}
+              alt="Screenshot 3"
+              className="rounded-xl shadow"
+            />
+          </div>
+        </section>
+
+        {/* FOOTER */}
+        <footer className="text-center text-sm text-gray-600 mt-12">
+          Created by Krishna — ☕{" "}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://www.buymeacoffee.com/krish264"
+            className="underline hover:text-gray-800"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            Read our docs
+            Buy me a coffee
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
