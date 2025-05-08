9a6644cf6091bb29c7f1ed9aa1538185196bdc15
Krishcode264 <krishnazade99@gmail.com>
2025-05-08 08:40:46 +0530

    SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://web.boldhug.krishcode264.online',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://web.boldhug.krishcode264.online',
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    return {
+      loc: `${config.siteUrl}${path}`,
+      changefreq: 'daily',
+      priority: path === '/' ? 1.0 : 0.7, // Set priority to 1.0 for homepage
+      lastmod: new Date().toISOString(),
+    };
+  },
+};
